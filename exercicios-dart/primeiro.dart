b4e980a319c39229ffc98391afcc6c2f5a6bf4c9
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-07-01 15:36:54 -0300

    função arrow

--- exercicios-dart/primeiro.dart
+++ exercicios-dart/primeiro.dart
@@ -1,13 +1,17 @@
 //inferencia em dart
 main() {
-  var soma = (x, y) {
+  var soma = ([x = 1, y = 1]) {
     return x + y;
   };
-  print(soma(1, 1));
+  print(soma());
 
 //Semelhante a
   int Function(int, int) soma1 = (x, y) {
     return x + y;
   };
   print(soma1(1, 10));
+
+//Função arrow
+  var soma2 = (int a, int b) => a + b;
+  print(soma2(10, 10));
 }
