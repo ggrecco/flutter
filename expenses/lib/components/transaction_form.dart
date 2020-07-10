ec1ca3b6f8019542cbe573971f8988bf13856383
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-07-10 19:24:37 -0300

    botao selecionar data chamando modal

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -25,10 +25,13 @@ class _TransactionFormState extends State<TransactionForm> {
     widget.onSubmit(title, value);
   }
 
-  //mostrar calendarios
+  //Selecionar datas
   _showDatePicker(){
     showDatePicker(
-
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
     );
   }
 
@@ -68,7 +71,7 @@ class _TransactionFormState extends State<TransactionForm> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: _showDatePicker,
                   )
                 ],
               ),
