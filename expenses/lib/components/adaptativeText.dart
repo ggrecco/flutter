b776190e68dd327b875b0641ca1a4148968f46fa
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-07-12 10:38:46 -0300

    minha resposta desafio adaptativeText

--- expenses/lib/components/adaptativeText.dart
+++ expenses/lib/components/adaptativeText.dart
@@ -3,32 +3,29 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 class AdaptativeText extends StatelessWidget {
-
   final String label;
   final TextEditingController controller;
   final Function submit;
-  final TextInputType keyboard;  
+  final TextInputType keyboard;
 
-  AdaptativeText({
-    this.controller,
-    this.submit,
-    this.label,
-    this.keyboard
-  });
+  AdaptativeText({this.controller, this.submit, this.label, this.keyboard});
 
   @override
   Widget build(BuildContext context) {
     return Platform.isIOS
-    ? CupertinoTextField(
-
-    )
-    : TextField(
-        controller: controller,
-        keyboardType: keyboard,
-        onSubmitted: submit,
-        decoration: InputDecoration(
-          labelText: label,
-        ),
-    );
+        ? CupertinoTextField(
+            controller: controller,
+            keyboardType: keyboard,
+            onSubmitted: submit,
+            placeholder: label,
+          )
+        : TextField(
+            controller: controller,
+            keyboardType: keyboard,
+            onSubmitted: submit,
+            decoration: InputDecoration(
+              labelText: label,
+            ),
+          );
   }
 }
