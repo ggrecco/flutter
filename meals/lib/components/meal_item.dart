dbcbbf6a24545b31947520a209caa826be72eeee
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-07-17 17:48:26 -0300

    importada imagens para o app e definida as bordas

--- meals/lib/components/meal_item.dart
+++ meals/lib/components/meal_item.dart
@@ -7,16 +7,38 @@ class MealItem extends StatelessWidget {
 
   const MealItem(this.meal);
 
+  void _selectMeal() {}
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: _selectMeal,
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
         ),
         elevation: 4,
         margin: const EdgeInsets.all(10),
-        child: Text(meal.title),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
