9c960d101c081a5876cb433a2bc82f213741dbd0
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-07-31 14:03:44 -0300

    refatorado codigo de grid

--- shop/lib/views/products_overview_screen.dart
+++ shop/lib/views/products_overview_screen.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_grid.dart';
+
+// import 'package:shop/models/product.dart';
+// import 'package:shop/providers/products.dart';
 // import '../models/product.dart';
-import '../widgets/product_item.dart';
+// import '../widgets/product_item.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final List<Product> loadedProducts = Provider.of<Products>(context).items;
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
 
     return Scaffold(
       appBar: AppBar(
-          title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Minha Loja'),
-        ],
-      )),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (ctx, i) => ProductItem(products[i]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Minha Loja'),
+          ],
         ),
       ),
+      body: ProductGrid(),
     );
   }
 }
