684dba618479e285830b6caccb5f4eb9ef195df3
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-01 12:32:15 -0300

    preparando class cart para remover item do cart

--- shop/lib/providers/cart.dart
+++ shop/lib/providers/cart.dart
@@ -5,12 +5,14 @@ import './product.dart';
 
 class CartItem {
   final String id;
+  final String productId;
   final String title;
   final int quanty;
   final double price;
 
   CartItem({
     @required this.id,
+    @required this.productId,
     @required this.title,
     @required this.price,
     @required this.quanty,
@@ -41,6 +43,7 @@ class Cart with ChangeNotifier {
       _items.update(product.id, (existinItem) {
         return CartItem(
           id: existinItem.id,
+          productId: product.id,
           price: existinItem.price,
           quanty: existinItem.quanty + 1,
           title: existinItem.title,
@@ -51,12 +54,18 @@ class Cart with ChangeNotifier {
         product.id,
         () => CartItem(
           id: Random().nextDouble().toString(),
+          productId: product.id,
           title: product.title,
           price: product.price,
           quanty: 1,
         ),
       );
     }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
     notifyListeners();
   }
 }
