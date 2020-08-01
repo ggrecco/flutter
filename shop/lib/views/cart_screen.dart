7fe38eada64aac1bdd105d0e0de565d35175d0ed
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-01 08:10:49 -0300

    layou do total das compras com 2 casas decimais

--- shop/lib/views/cart_screen.dart
+++ shop/lib/views/cart_screen.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import '../providers/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(25),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(width: 10),
+                  Chip(
+                    label: Text(
+                      'R\$ ${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6.color,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  Spacer(),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text('COMPRAR'),
+                    textColor: Theme.of(context).primaryColor,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
