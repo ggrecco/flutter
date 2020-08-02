79416be3170f09006444e9115eac1958a30f08a7
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-02 00:28:59 -0300

    cirado card para os pedidos

--- shop/lib/widgets/order_widget.dart
+++ shop/lib/widgets/order_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import '../providers/orders.dart';
 
 class OrderWidget extends StatelessWidget {
@@ -8,9 +10,17 @@ class OrderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        margin: EdgeInsets.all(10),
-        child: ListTile(
-          title: Text('R\$ ${order.total}'),
-        ));
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        title: Text('R\$ ${order.total}'),
+        subtitle: Text(
+          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.expand_more),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
