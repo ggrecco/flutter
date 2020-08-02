2545e4c89e1a92253b9691283586781ab58b4f05
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-01 22:14:36 -0300

    criando provider para pedido

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -6,6 +6,7 @@ import './utils/app_route.dart';
 import './views/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Orders(),
         ),
       ],
       child: MaterialApp(
