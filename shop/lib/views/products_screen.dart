7a619fdc7a38d8208d258b92b76cfdcccebd64ed
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-03 12:55:00 -0300

    cirado icones de editar e excluir produto

--- shop/lib/views/products_screen.dart
+++ shop/lib/views/products_screen.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/app_drawe.dart';
+import '../widgets/product_item.dart';
 import '../providers/products.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final products = Provider.of<Products>(context);
+    final productsItems = products.items;
     return Scaffold(
         appBar: AppBar(
           title: Text('Gerenciar Produtos'),
@@ -23,7 +25,14 @@ class ProductsScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: ListView.builder(
             itemCount: products.itemCount,
-            itemBuilder: (ctx, i) => Text('teste'),
+            itemBuilder: (ctx, i) => Column(
+              children: <Widget>[
+                ProductItem(
+                  product: productsItems[i],
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ));
   }
