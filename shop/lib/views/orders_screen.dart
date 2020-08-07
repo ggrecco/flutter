30a3109e8392100d9a0557ace64bff6470c926cb
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-06 21:07:23 -0300

    refatorando p usar FutureBuilder

--- shop/lib/views/orders_screen.dart
+++ shop/lib/views/orders_screen.dart
@@ -4,41 +4,43 @@ import '../providers/orders.dart';
 import 'package:provider/provider.dart';
 import '../widgets/order_widget.dart';
 
-class OrderScreen extends StatefulWidget {
-  @override
-  _OrderScreenState createState() => _OrderScreenState();
-}
-
-class _OrderScreenState extends State<OrderScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    Provider.of<Orders>(context, listen: false).loadOrders().then((_) {
-          setState(() {
-            _isLoading = false;
-          });
-        });
-  }
-
+class OrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Orders orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.itemCount,
-              itemBuilder: (ctx, i) => OrderWidget(
-                order: orders.items[i],
-              ),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null){
+            return Center(child: Text('Ocoreu um erro inesperado'));
+          } else {
+            return Consumer<Orders>(
+              builder: (ctx, orders, child) {
+                return ListView.builder(
+                  itemCount: orders.itemCount,
+                  itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+                );
+              },
+            );
+          }
+        },
+      ),
+      //  _isLoading
+      //     ? Center(
+      //         child: CircularProgressIndicator(),
+      //       )
+      //     : ListView.builder(
+      //         itemCount: orders.itemCount,
+      //         itemBuilder: (ctx, i) => OrderWidget(
+      //           order: orders.items[i],
+      //         ),
+      //       ),
     );
   }
 }
