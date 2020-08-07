c57948936b4d076af0bd52cb5efa4128e281ea72
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-06 21:34:53 -0300

    dafinido background da tela de autenticação

--- shop/lib/views/auth_screen.dart
+++ shop/lib/views/auth_screen.dart
@@ -4,8 +4,29 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('teste'),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 177, 255, 0.5),
+                  Color.fromRGBO(255, 188, 117, 0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.topRight,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
