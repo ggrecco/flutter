adf95dc55c91f7009edbd3fa37982ae7fe591989
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-06 22:15:07 -0300

    criado campo de e-mail no formulario

--- shop/lib/widgets/auth_card.dart
+++ shop/lib/widgets/auth_card.dart
@@ -20,7 +20,23 @@ class _AuthCardState extends State<AuthCard> {
       child: Container(
         height: 320,
         width: deviceSize.width * 0.75,
-        child: Text('card'),
+        padding: EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if(value.isEmpty || !value.contains('@') || !value.contains('.com')){
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
