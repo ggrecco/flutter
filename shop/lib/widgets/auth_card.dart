cb445f296e24a1bf590988b34589af50face2307
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-06 22:08:20 -0300

    criado card com bordas arredondadas

--- shop/lib/widgets/auth_card.dart
+++ shop/lib/widgets/auth_card.dart
@@ -10,9 +10,18 @@ class AuthCard extends StatefulWidget {
 class _AuthCardState extends State<AuthCard> {
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
     return Card(
-      elevation: 8.0, 
-      child: Text('Card'),
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        height: 320,
+        width: deviceSize.width * 0.75,
+        child: Text('card'),
+      ),
     );
   }
 }
