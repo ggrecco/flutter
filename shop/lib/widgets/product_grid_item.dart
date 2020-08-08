6adb88114ec78eb77789f38f631a4656daa34687
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-08 15:37:25 -0300

    token nos favoritos

--- shop/lib/widgets/product_grid_item.dart
+++ shop/lib/widgets/product_grid_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/providers/auth.dart';
 import '../providers/product.dart';
 import '../providers/cart.dart';
 import '../utils/app_route.dart';
@@ -9,6 +10,7 @@ class ProductGridItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final Product product = Provider.of<Product>(context, listen: false);
     final Cart cart = Provider.of<Cart>(context, listen: false);
+    final Auth auth = Provider.of<Auth>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -30,7 +32,7 @@ class ProductGridItem extends StatelessWidget {
             builder: (ctx, product, _) => IconButton(
               icon: Icon(
                   product.isFavorite ? Icons.favorite : Icons.favorite_border),
-              onPressed: () => product.toggleFavorite(),
+              onPressed: () => product.toggleFavorite(auth.token),
               color: Theme.of(context).accentColor,
             ),
           ),
