77bfe3a37ba943c752716d532a22a6732c01295c
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-08-08 00:29:27 -0300

    criado metodo para verificar se o token

--- shop/lib/providers/auth.dart
+++ shop/lib/providers/auth.dart
@@ -5,14 +5,24 @@ import 'package:http/http.dart' as http;
 import 'package:shop/exception/auth_exception.dart';
 
 class Auth with ChangeNotifier {
-      // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  String _token;
+  DateTime _expiryDate;
 
+  String get token {
+    if (token != null &&
+        _expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now())) {
+      return token;
+    } else {
+      return null;
+    }
+  }
 
-
-  Future<void> _authenticate(String email, String password, String urlSegment) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:${urlSegment}?key=AIzaSyAssHH1IKsQx06-LLh33TC0W7hGTY6iyfM';
-    
+        'https://identitytoolkit.googleapis.com/v1/accounts:${urlSegment}?key=AIzaSyAssHH1IKsQx06-LLh33TC0W7hGTY6iyfM';
+
     final response = await http.post(
       url,
       body: json.encode({
@@ -23,12 +33,19 @@ class Auth with ChangeNotifier {
     );
 
     final responseBody = json.decode(response.body);
-    if(responseBody['error'] != null){
+    if (responseBody['error'] != null) {
       throw AuthException(responseBody['error']['message']);
+    } else {
+      _token = responseBody['idToken'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseBody['expiresIn']),
+        ),
+      );
     }
-    
+
     return Future.value();
-  }    
+  }
 
   Future<void> signup(String email, String password) async {
     return _authenticate(email, password, 'signUp');
@@ -37,5 +54,4 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
-
 }
