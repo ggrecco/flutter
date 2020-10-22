60eaf6f499fcd5785103c5588e1d8b71a8f5a3f1
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-10-22 06:04:28 -0300

    animação amis facilitada

--- shop/lib/widgets/auth_card.dart
+++ shop/lib/widgets/auth_card.dart
@@ -126,87 +126,84 @@ class _AuthCardState extends State<AuthCard>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: AnimatedBuilder(
-        animation: _heightAmation,
-        child: Form(
-            key: _form,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty ||
-                        !value.contains('@') ||
-                        !value.contains('.com')) {
-                      return 'Informe um e-mail válido';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _authData['email'] = value,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Senha'),
-                  controller: _passwordController,
-                  obscureText: true,
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 5) {
-                      return 'Informe uma senha maior que 5 caracteres';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _authData['password'] = value,
-                ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Confirmar Senha'),
-                    obscureText: true,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Senhas diferentes!';
-                            }
-                            return null;
-                          }
-                        : null,
-                  ),
-                SizedBox(height: 20),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).primaryTextTheme.button.color,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 30.0,
-                      vertical: 8.0,
-                    ),
-                    child: Text(
-                      _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
-                    ),
-                    onPressed: _submit,
-                  ),
-                FlatButton(
-                  onPressed: _switchAuthMode,
-                  child: Text(
-                    'ALTERNAR P/ ${_authMode == AuthMode.Login ? 'REGISTRAR' : 'ENTRAR'}',
-                  ),
-                  textColor: Theme.of(context).primaryColor,
-                ),
-              ],
-            ),
-          ),
-        builder: (ctx, child) => Container(
-          // height: _authMode == AuthMode.Login ? 310 : 391,
-          height: _heightAmation.value.height,
+      child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeIn,
+          height: _authMode == AuthMode.Login ? 290 : 375,
           width: deviceSize.width * 0.75,
           padding: EdgeInsets.all(16),
-          child: child,
+          child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty ||
+                      !value.contains('@') ||
+                      !value.contains('.com')) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _authData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Senha'),
+                controller: _passwordController,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty || value.length < 5) {
+                    return 'Informe uma senha maior que 5 caracteres';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _authData['password'] = value,
+              ),
+              if (_authMode == AuthMode.Signup)
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: _authMode == AuthMode.Signup
+                      ? (value) {
+                          if (value != _passwordController.text) {
+                            return 'Senhas diferentes!';
+                          }
+                          return null;
+                        }
+                      : null,
+                ),
+              SizedBox(height: 20),
+              if (_isLoading)
+                CircularProgressIndicator()
+              else
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).primaryTextTheme.button.color,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                    vertical: 8.0,
+                  ),
+                  child: Text(
+                    _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
+                  ),
+                  onPressed: _submit,
+                ),
+              FlatButton(
+                onPressed: _switchAuthMode,
+                child: Text(
+                  'ALTERNAR P/ ${_authMode == AuthMode.Login ? 'REGISTRAR' : 'ENTRAR'}',
+                ),
+                textColor: Theme.of(context).primaryColor,
+              ),
+            ],
+          ),
         ),
       ),
     );
